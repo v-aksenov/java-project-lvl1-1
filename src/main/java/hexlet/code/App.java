9402f69a714dcf6a3e210db80879a1ae07677078
sergye <sergye@gmail.com>
2021-05-23 14:53:09 +0300

    Step 5 - add game choice

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greetGameUser();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+
+        Scanner scan = new Scanner(System.in);
+        int userChoice = Integer.parseInt(scan.nextLine());
+
+        System.out.println("Your choice: " + userChoice);
+
+        if (userChoice == 1) {
+            Cli.greetGameUser();
+        }
     }
 }
