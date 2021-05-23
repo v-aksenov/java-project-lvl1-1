3b1006e0a339aa5b812ec5aae268cb18db465eed
sergye <sergye@gmail.com>
2021-05-23 15:46:36 +0300

    Step 5 - change to return user name

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,14 +3,14 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greetGameUser() {
+    public static String greetGameUser() {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
 
         Scanner scan = new Scanner(System.in);
         String userName = scan.nextLine();
-
         System.out.println("Hello, " + userName + "!");
 
+        return userName;
     }
 }
