0a6db2be35e28543a6118616c349527151ab78f4
sergye <sergye@gmail.com>
2021-05-23 14:58:08 +0300

    Step 5 - remove carriage return

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greetGameUser() {
         System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
+        System.out.print("May I have your name? ");
 
         Scanner scan = new Scanner(System.in);
         String userName = scan.nextLine();
