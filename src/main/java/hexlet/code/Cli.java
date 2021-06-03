05dc871860a496db1dea1e5c969e17cba3f2b0d1
sergye <sergye@gmail.com>
2021-06-03 12:58:07 +0300

    Step 6 - refactor Cli.java

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,13 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String greetGameUser() {
+    public static String getUserName() {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
 
         Scanner scan = new Scanner(System.in);
         String userName = scan.nextLine();
-        System.out.println("Hello, " + userName + "!");
 
         return userName;
     }
